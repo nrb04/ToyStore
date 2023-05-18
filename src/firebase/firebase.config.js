05b3434b2642a0dbac48731647855f796a2f3e20
nrb04 <nredoyan@gmail.com>
2023-05-18 12:01:39 +0600

    client to folder

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCM58uSwEXlE9HMUyJe5IUyuD_y3hJ0zSA",
+  authDomain: "ass11-2023.firebaseapp.com",
+  projectId: "ass11-2023",
+  storageBucket: "ass11-2023.appspot.com",
+  messagingSenderId: "644005053200",
+  appId: "1:644005053200:web:251592a92d63cd8a54db95",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+
 import "./index.css";
 import { Carousel, initTE } from "tw-elements";
 initTE({ Carousel }, true);
-
+import { RouterProvider } from "react-router-dom";
+import router from "./route/Router.jsx";
+import { AuthenticationProvider } from "react-auth-provider";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {" "}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
